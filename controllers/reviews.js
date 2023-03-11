f42ef7d9afbd00c93b5b7672f09e1928550e7e31
branhickman <branhickman@gmail.com>
2023-03-10 20:46:10 -0700

    setup review index controller function and associated route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -8,7 +8,6 @@ async function create(req, res) {
     const review = await Review.create(req.body)
     res.status(200).json(review)
   } catch (error) {
-    console.log(error)
     res.status(500).json({ err: error })
   }
 }
